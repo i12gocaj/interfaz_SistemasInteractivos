0602e19cd8547677d63f6fad781b8391bf5df219
Jesus-Nunez-de-Arenas <jenudearlla@gmail.com>
2024-06-01 20:04:10 +0200

    deleted duplicates of rounded botton class

--- interfaz/LoginFrame.java
+++ interfaz/LoginFrame.java
@@ -15,28 +15,6 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 // Clase para botones redondeados
-class RoundedButton extends JButton {
-    public RoundedButton(Icon icon) {
-        super(icon);
-        setBorderPainted(false);
-        setContentAreaFilled(false);
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        if (getModel().isPressed()) {
-            g.setColor(new Color(0, 0, 0, 100));
-            g.fillRoundRect(0, 0, getWidth(), getHeight(), 20, 20);
-        }
-        super.paintComponent(g);
-    }
-
-    @Override
-    protected void paintBorder(Graphics g) {
-        // No pintar borde
-    }
-}
-
 public class LoginFrame extends JFrame {
 
     public LoginFrame(ResourceBundle bundle_text, ArrayList<String> roomNames, List<String[]> objectConfigurations) {
