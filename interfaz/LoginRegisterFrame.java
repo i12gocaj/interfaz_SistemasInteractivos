190a43123583c60a72bbde1d2d628a65ba617579
Jesus-Nunez-de-Arenas <jenudearlla@gmail.com>
2024-06-01 20:43:17 +0200

    created a .java with roundedButton

--- interfaz/LoginRegisterFrame.java
+++ interfaz/LoginRegisterFrame.java
@@ -12,29 +12,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
-// Clase para botones redondeados
-class RoundedButton extends JButton {
-    public RoundedButton(Icon icon) {
-        super(icon);
-        setBorderPainted(false);
-        setContentAreaFilled(false);
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        if (getModel().isPressed()) {
-            g.setColor(new Color(0, 0, 0, 100));
-            g.fillRoundRect(0, 0, getWidth(), getHeight(), 20, 20);
-        }
-        super.paintComponent(g);
-    }
-
-    @Override
-    protected void paintBorder(Graphics g) {
-        // No pintar borde
-    }
-}
-
 public class LoginRegisterFrame extends JFrame {
 
     // Constructor de la clase
---
+++ interfaz/RoundedButton.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class RoundedButton extends JButton {
+    public RoundedButton(Icon icon) {
+        super(icon);
+        setBorderPainted(false);
+        setContentAreaFilled(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (getModel().isPressed()) {
+            g.setColor(new Color(0, 0, 0, 100));
+            g.fillRoundRect(0, 0, getWidth(), getHeight(), 20, 20);
+        }
+        super.paintComponent(g);
+    }
+
+    @Override
+    protected void paintBorder(Graphics g) {
+        // No pintar borde
+    }
+}
